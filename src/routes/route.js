61ba2975f12d178d73cf62b03c264a9cce7ad29e
Tushar Sahni <tusharsahni22@gmai.com>
2023-05-16 15:06:18 +0530

    body parser added

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,12 @@
 const express = require("express")
+bodyParser = require('body-parser')
 const router = express.Router();
 const {singUpUser , login } = require("../controller/auth")
 const {addPost,UpdatePost,getPost} =require("../controller/Post")
 const {authTokenCheck} = require("../middleware/middleware")
 
 router.use(express.json())
+router.use(bodyParser.json())
 
 router.route("/signup").post(singUpUser)
 router.route("/login").get(login)
